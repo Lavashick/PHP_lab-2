ca8925316d977918780b310717c35ddc05ceddae
Lavashick <lavashick@icloud.com>
2021-04-23 02:27:21 +0300

    Функция получения данных в таблице

--- main.php
+++ main.php
@@ -14,5 +14,22 @@ $tables = [
     "C11*C12*C13#C14*C15*C16"
 ];
 
+function getTR($data, $columns_num) {
+    if ($columns_num == 0) return 'Неправильное число колонок';
+
+    $arr = explode( '*', $data);
+    if (count($arr) == 1 && $arr[0] == '')
+        return '<tr><td>В строке нет ячеек</td></tr>';
+
+    $ret = '';
+    
+    for ($i = 0; $i < $columns_num; $i++)
+        if ($arr[$i] != '') {
+            $ret .= '<td>' . $arr[$i] . '</td>';
+        } else
+            $ret .= '<td></td>';
+    return '<tr>'.$ret.'</tr>';
+}
+
 
 ?>
