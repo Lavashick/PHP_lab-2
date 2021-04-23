e4bf146ca6443bb057f1c3640324c96dfd652f46
Lavashick <lavashick@icloud.com>
2021-04-23 04:24:23 +0300

    Выполнена основная логика и вывод. Проблемы с табличным выводом

--- header.php
+++ header.php
@@ -5,6 +5,13 @@
             <a class="navbar-brand" href="#">
                 <img src="img/logo.png" alt="" height=80 class="d-inline-block align-text-top">
             </a>
+            <a class="navbar" href="#">
+                Блочная верстка
+            </a>
+
+            <a class="navbar" href="#">
+                Табличная верстка
+            </a>
             
             <form class="d-flex signature">
                 Сметанина Александра<br>
--- index.php
+++ index.php
@@ -16,15 +16,12 @@
     <?php include("header.php") ?>
 
     <main role="main" class="flex-shrink-0 mb-5">
-
         <div class="container-fluid row">
             <?php
             $start_multi = 2;
             $end_multi = 9;
+
             ?>
-
-            <?php include("main.php") ?>
-
             <div class="col-3">
                 <?php
                 echo '<ul class="left-menu">';
@@ -51,10 +48,77 @@
                 }
                 ?>
             </div>
+            <div class="col">
+                <?php
+                if (isset($_GET['html_type']) && $_GET['html_type'] == 'TABLE')
+                    echo tableForm($start_multi, $end_multi);
+                else
+                    echo divForm($start_multi, $end_multi);
+                ?>
+            </div>
+
+            <?php
+
+            function tableForm($start, $end)
+            {
+                $res = '';
+                if (!isset($_GET['content'])) {
+                    for ($i = $start; $i <= $end; $i++) {
+                        $res .= '<tr>' . outRow($i, $start, $end) . '</tr>';
+                    }
+                } else {
+                    $x = $_GET['content'];
+                    $res .= '<tr>';
+                    for ($i = $start; $i <= $end; $i++) {
+                        $res .= '<td>' . outStr($x, $i, $end) . '</td>';
+                    }
+                    $res .= '</tr>';
+                }
+                return '<table class="table table-striped table-dark table-bordered">' . $res . '</table>';
+            }
+
+            function divForm($start, $end)
+            {
+                $res = '<div class="d-flex flex-column">';
+                if (!isset($_GET['content'])) {
+                    for ($i = $start; $i <= $end; $i++) {
+                        $res .= '<div class="d-flex justify-content-around m-3">';
+                        $res .= outRow($i, $start, $end);
+                        $res .= '</div>';
+                    }
+                } else {
+                    $res .= '<div class="d-flex justify-content-around m-3">';
+                    $res .= outRow($_GET['content'], $start, $end);
+                    $res .= '</div>';
+                }
+                return $res . '</div>';
+            }
+
+            function outRow($n, $start, $end)
+            {
+                $res = '';
+                for ($i = $start; $i <= $end; $i++) {
+                    if (!isset($_GET['html_type']) || $_GET['html_type'] == 'TABLE')
+                        $res .= '<td>' . outStr($i, $n, $end) . '<br></td>';
+                    else
+                        $res .= '<div>' . outStr($i, $n, $end) . '</div>' . '<br>';
+                }
+                return $res;
+            }
+
+            function outStr($x, $y, $end)
+            {
+                $res = ($x * $y <= $end) ? outNumAsLink($x * $y) : $x * $y;
+                return outNumAsLink($x) . ' x ' . outNumAsLink($y) . ' = ' . $res;
+            }
+
+            function outNumAsLink($n)
+            {
+                $res = '<a href="?content=' . $n;
+                return $res . '">' . $n . '</a>';
+            }
+            ?>
         </div>
-
-
-
     </main>
 
 
