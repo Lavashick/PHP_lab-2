1c9116aa523218fe907a14fd117d4f77dd757967
Lavashik <lavashick@icloud.com>
2021-04-24 23:37:55 +0300

    Сделаны кнопки выбора вывода данных

--- header.php
+++ header.php
@@ -5,12 +5,12 @@
             <a class="navbar-brand" href="#">
                 <img src="img/logo.png" alt="" height=80 class="d-inline-block align-text-top">
             </a>
-            <a class="navbar" href="#">
-                Блочная верстка
+            <a class="navbar" href="/?html_type=TABLE<?php if (isset($content)) echo '&content=' . $content ?>">
+                Табличная верстка
             </a>
 
-            <a class="navbar" href="#">
-                Табличная верстка
+            <a class="navbar" href="/?html_type=DIV<?php if (isset($content)) echo '&content=' . $content; ?>">
+                Блочная верстка
             </a>
             
             <form class="d-flex signature">
--- index.php
+++ index.php
@@ -12,34 +12,43 @@
     <link rel="stylesheet" href="style.css">
 </head>
 
+<?php
+$start_multi = 2;
+$end_multi = 9;
+
+$contents = array();
+for ($i = $start_multi; $i <= $end_multi; $i++)
+    $contents[$i - $start_multi] = $i;
+$content = (int)$_GET['content'];
+if (!in_array($content, $contents))
+    $content = null;
+$html_type = $_GET['html_type'];
+?>
+
 <body>
     <?php include("header.php") ?>
 
     <main role="main" class="flex-shrink-0 mb-5">
         <div class="container-fluid row">
-            <?php
-            $start_multi = 2;
-            $end_multi = 9;
-
-            ?>
             <div class="col-3">
                 <?php
                 echo '<ul class="left-menu">';
 
-                createElement('Вся таблица умножения', null, !isset($_GET['content']));
+                createElement('Вся таблица умножения', null, !isset($content));
 
                 for ($i = $start_multi; $i <= $end_multi; $i++) {
-                    $isActive = array_key_exists('content', $_GET) && $_GET['content'] == $i;
+                    $isActive = $content == $i;
                     createElement('Таблица умножения на ' . $i, $i, $isActive);
                 }
 
                 function createElement($title, $number, $isActive)
                 {
+                    global $html_type;
                     echo '<li><a href="?';
                     if ($number !== null)
                         echo '&content=' . $number;
-                    if (isset($_GET['html_type']))
-                        echo '&html_type=' . $_GET['html_type'];
+                    if (isset($html_type))
+                        echo '&html_type=' . $html_type;
                     echo '"';
                     if ($isActive)
                         echo ' class="active"';
@@ -50,7 +59,7 @@
             </div>
             <div class="col">
                 <?php
-                if (isset($_GET['html_type']) && $_GET['html_type'] == 'TABLE')
+                if (!isset($html_type) || $html_type == 'TABLE')
                     echo tableForm($start_multi, $end_multi);
                 else
                     echo divForm($start_multi, $end_multi);
@@ -61,26 +70,28 @@
 
             function tableForm($start, $end)
             {
+                global $content;
                 $res = '';
-                if (!isset($_GET['content'])) {
+                if (!isset($content)) {
                     for ($i = $start; $i <= $end; $i++) {
                         $res .= '<tr>' . outRow($i, $start, $end) . '</tr>';
                     }
                 } else {
-                    $x = $_GET['content'];
+                    $x = $content;
                     $res .= '<tr>';
                     for ($i = $start; $i <= $end; $i++) {
                         $res .= '<td>' . outStr($x, $i, $end) . '</td>';
                     }
                     $res .= '</tr>';
                 }
-                return '<table class="table table-striped table-dark table-bordered">' . $res . '</table>';
+                return '<table class="table table-striped table-dark">' . $res . '</table>';
             }
 
             function divForm($start, $end)
             {
+                global $content;
                 $res = '<div class="d-flex flex-column">';
-                if (!isset($_GET['content'])) {
+                if (!isset($content)) {
                     for ($i = $start; $i <= $end; $i++) {
                         $res .= '<div class="d-flex justify-content-around m-3">';
                         $res .= outRow($i, $start, $end);
@@ -88,7 +99,7 @@
                     }
                 } else {
                     $res .= '<div class="d-flex justify-content-around m-3">';
-                    $res .= outRow($_GET['content'], $start, $end);
+                    $res .= outRow($content, $start, $end);
                     $res .= '</div>';
                 }
                 return $res . '</div>';
@@ -96,9 +107,10 @@
 
             function outRow($n, $start, $end)
             {
+                global $html_type;
                 $res = '';
                 for ($i = $start; $i <= $end; $i++) {
-                    if (!isset($_GET['html_type']) || $_GET['html_type'] == 'TABLE')
+                    if (!isset($html_type) || $html_type == 'TABLE')
                         $res .= '<td>' . outStr($i, $n, $end) . '<br></td>';
                     else
                         $res .= '<div>' . outStr($i, $n, $end) . '</div>' . '<br>';
@@ -108,6 +120,7 @@
 
             function outStr($x, $y, $end)
             {
+                echo $x;
                 $res = ($x * $y <= $end) ? outNumAsLink($x * $y) : $x * $y;
                 return outNumAsLink($x) . ' x ' . outNumAsLink($y) . ' = ' . $res;
             }
